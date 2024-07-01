1bb4383c095fa400f87d55c74a923910140224de
Isen <3104255643@qq.com>
2024-07-01 16:14:19 +0800

    feat: 初始化 （ts,husky等）

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  $schema: 'https://json.schemastore.org/prettierrc',
+  semi: true, // * 结尾使用分号
+  tabWidth: 2, // * tab缩进大小,默认为2
+  singleQuote: true, // * 使用单引号
+  trailingComma: 'none',
+  printWidth: 120, // * 单行输出（不折行）的（最大）长度
+  arrowParens: 'avoid', // * 箭头函数参数括号
+  proseWrap: 'always' // * 当超出print width时就折行
+};
