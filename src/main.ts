9e5c1abcd1094d98de171f512c1fb6556f371865
Isen <3104255643@qq.com>
2024-07-01 18:06:09 +0800

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './assets/main.css';
-
+import 'element-plus/es/components/message/style/css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,54 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+// import Inspect from 'vite-plugin-inspect';
+
+// * 自动导入
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    // Inspect(),
+    // * 自动导入，vue vue-router
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+
+      // global imports to register
+      imports: ['vue', 'vue-router', '@vueuse/core'],
+
+      // Filepath to generate corresponding .d.ts file.
+      // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
+      // Set `false` to disable.
+      dts: './auto-imports.d.ts',
+
+      // Custom resolvers, compatible with `unplugin-vue-components`
+      // see https://github.com/antfu/unplugin-auto-import/pull/23/
+      resolvers: [ElementPlusResolver()],
+
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json' // Default `./.eslintrc-auto-import.json`
+      },
+      vueTemplate: true
+    }),
+    Components({
+      dts: true,
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
